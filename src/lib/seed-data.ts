d7d5f595c9bccb766dc8954e513215fa814e9baf
duyan-pb <duyan.tran@polymerbionics.com>
2025-12-17 06:08:29 +0000

    Generated by Spark: please remove half of the images that looks duplicate for the inear dive

--- src/lib/seed-data.ts
+++ src/lib/seed-data.ts
@@ -3,26 +3,16 @@ import { materials, applications } from './materials-data'
 
 import pxl1 from '@/assets/images/PXL_20251216_115711682.PORTRAIT.jpg'
 import pxl2 from '@/assets/images/PXL_20251216_115728419.PORTRAIT.ORIGINAL.jpg'
-import pxl3 from '@/assets/images/PXL_20251216_115737523.PORTRAIT.jpg'
-import pxl4 from '@/assets/images/PXL_20251216_115759988.jpg'
 import pxl5 from '@/assets/images/PXL_20251216_115810661.jpg'
-import pxl6 from '@/assets/images/PXL_20251216_115825489.jpg'
 import pxl7 from '@/assets/images/PXL_20251216_115836016.jpg'
-import pxl8 from '@/assets/images/PXL_20251216_115843208.jpg'
 import pxl9 from '@/assets/images/PXL_20251216_115854143.jpg'
-import pxl10 from '@/assets/images/PXL_20251216_115905670.jpg'
 import pxl11 from '@/assets/images/PXL_20251216_115937300.jpg'
-import pxl12 from '@/assets/images/PXL_20251216_115945238.jpg'
 import pxl13 from '@/assets/images/PXL_20251216_115958662.jpg'
-import pxl14 from '@/assets/images/PXL_20251216_120055171.jpg'
 import pxl15 from '@/assets/images/PXL_20251216_120056575.jpg'
-import pxl16 from '@/assets/images/PXL_20251216_120105654.jpg'
 import pxl17 from '@/assets/images/PXL_20251216_120112987.jpg'
-import pxl18 from '@/assets/images/PXL_20251216_120119941.jpg'
 
 const inEarDeviceImages = [
-  pxl1, pxl2, pxl3, pxl4, pxl5, pxl6, pxl7, pxl8, pxl9,
-  pxl10, pxl11, pxl12, pxl13, pxl14, pxl15, pxl16, pxl17, pxl18
+  pxl1, pxl2, pxl5, pxl7, pxl9, pxl11, pxl13, pxl15, pxl17
 ]
 
 export async function generateBiomaterialsProducts(): Promise<Product[]> {
